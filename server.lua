8641dd775bd187bce20e12cb0cc08bcfbc8ac21d
storm37000 <6331913+storm37000@users.noreply.github.com>
2020-11-19 19:23:32 -0500

    remove esx depend

--- server.lua
+++ server.lua
@@ -1,34 +1,30 @@
+local trainspawned = false
+local PlayerCount = 0
+local list = {}
 
-PlayerCount = 0
-list = {}
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-RegisterServerEvent("esx:playerLoaded")
+RegisterServerEvent("hardcap:playerActivated")
 RegisterServerEvent("playerDropped")
 
-function ActivateTrain ()
-	local xPlayers = ESX.GetPlayers()
-	for i=1, #xPlayers, 1 do
-		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-
-		if (PlayerCount) == 1 then
-			TriggerClientEvent('StartTrain', xPlayer.source, GetHostId())
-		else
-			SetTimeout(15000, ActivateTrain)
+local function ActivateTrain ()
+	if (PlayerCount) == 1 and not trainspawned then
+		TriggerClientEvent('StartTrain', GetHostId())
+		trainspawned = true
+	else
+		SetTimeout(15000,ActivateTrain)
+		if (PlayerCount) == 0 then
+			trainspawned = false
 		end
 	end
 end
 --snippet from hardcap to make PlayerCount work
 
 -- yes i know i'm lazy
-AddEventHandler('esx:playerLoaded', function()
+AddEventHandler('hardcap:playerActivated', function()
   if not list[source] then
     PlayerCount = PlayerCount + 1
     list[source] = true
 		if (PlayerCount) == 1 then -- new session?
-			SetTimeout(15000, ActivateTrain)
+			SetTimeout(15000,ActivateTrain)
 		end
   end
 end)
